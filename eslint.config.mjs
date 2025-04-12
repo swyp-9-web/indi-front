da213839ec1cd65a0acb5b8cd50e1a871631ce56
leehj322 <gudwns8956@naver.com>
2025-04-13 00:52:16 +0900

    chore(eslint): ESLint 설정 수정 및 관련 라이브러리 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,63 @@
-export default [
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { dirname } from 'path';
+import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
   {
-    ignores: ['node_modules', '.next', 'dist', 'out', 'public'],
+    // Ignore generated, build, and dependency directories
+    ignores: ['.next/**', '**/*.d.ts', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'plugin:prettier/recommended'),
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      'no-implicit-coercion': 'error',
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-unused-vars': 'off',
+      'no-warning-comments': [
+        'warn',
+        {
+          terms: ['TODO', 'FIXME', 'XXX', 'BUG'],
+          location: 'anywhere',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-inferrable-types': 'warn',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          selector: 'variable',
+          leadingUnderscore: 'allow',
+        },
+        { format: ['camelCase', 'PascalCase'], selector: 'function' },
+        { format: ['PascalCase'], selector: 'interface' },
+        { format: ['PascalCase'], selector: 'typeAlias' },
+      ],
+      'prefer-object-has-own': 'error',
+    },
   },
 ];
+
+export default eslintConfig;
