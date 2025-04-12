3c355506d46fad5b67841a5aaebd6b94f50b2bfc
OK <ohl2619@naver.com>
2025-04-12 18:50:21 +0900

    chore(feat): deleted commit huskys files

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-// commitlint.config.js
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
-  },
-};
