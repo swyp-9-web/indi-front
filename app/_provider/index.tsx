ad8b308aded3a0feca279b4c9f88d4c8550abbc0
leehj322 <gudwns8956@naver.com>
2025-04-20 22:39:41 +0900

    feat(provider): add theme provider with next-themes

--- app/_provider/index.tsx
+++ app/_provider/index.tsx
@@ -1,5 +1,11 @@
 import QueryProvider from '@/app/_provider/queryProvider';
 
+import ThemeProvider from './ThemeProvider';
+
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <ThemeProvider>
+      <QueryProvider>{children}</QueryProvider>
+    </ThemeProvider>
+  );
 }
