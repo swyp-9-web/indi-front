b62a1fad7670d520cf46aa0268d775a67005a769
leehj322 <gudwns8956@naver.com>
2025-04-23 23:55:59 +0900

    refactor(routes): 페이지 컴포넌트 사용 규칙 변경 반영

--- app/(routes)/(main)/page.tsx
+++ app/(routes)/(main)/page.tsx
@@ -2,14 +2,8 @@
 
 import { useHelloQuery } from '@/lib/queries/useHelloQueries';
 
-import HomePage from './_components/Homepage';
-
 export default function Home() {
   const { data } = useHelloQuery();
 
-  return (
-    <>
-      <HomePage />
-    </>
-  );
+  return <div>{data?.message}</div>;
 }
