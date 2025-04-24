410224d8c543a7fee5eae72695216196f730b871
leehj322 <gudwns8956@naver.com>
2025-04-24 17:27:12 +0900

    feat(svgr): 네비게이션 바에 필요한 아이콘 추가

---
+++ lib/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as BookmarkIcon } from './svg/bookmark.svg';
+export { default as NotificationUnreadIcon } from './svg/notifications-unread.svg';
+export { default as NotificationIcon } from './svg/notifications.svg';
+export { default as SearchIcon } from './svg/search.svg';
