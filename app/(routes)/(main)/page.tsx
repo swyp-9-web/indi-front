138e11e51481a827d0e40f94e7c59ebbeac6b6f0
leehj322 <gudwns8956@naver.com>
2025-04-26 14:01:06 +0900

    feat(mainpage): 메인 페이지 DefaultProductsSection 추가

--- app/(routes)/(main)/page.tsx
+++ app/(routes)/(main)/page.tsx
@@ -1,5 +1,10 @@
-import Footer from '@/app/_components/shared/Footer';
+import Link from 'next/link';
 
+import Footer from '@/app/_components/shared/Footer';
+import { ROUTE_PATHS } from '@/constants';
+
+import CategoryFilter from './_components/CategoryFilter';
+import DefaultProductsSection from './_components/DefaultProductsSection';
 import HeroSection from './_components/HeroSection';
 
 export default function Home() {
@@ -8,6 +13,21 @@ export default function Home() {
       <div className="pt-14" />
 
       <HeroSection />
+
+      <section className="w-8xl mx-auto mt-25 px-20">
+        <CategoryFilter />
+        <DefaultProductsSection />
+      </section>
+
+      <section className="w-8xl mx-auto mt-25 px-20">
+        <DefaultProductsSection />
+        <Link
+          href={ROUTE_PATHS.PRODUCTS}
+          className="border-custom-gray-100 mx-auto mt-12.5 flex h-12 w-29 items-center justify-center rounded-full border text-sm font-medium"
+        >
+          모두보기
+        </Link>
+      </section>
 
       <Footer className="mt-27.5" />
     </>
