be6a2fe97d02c837894dc5fd83fcecbcddc7b676
leehj322 <gudwns8956@naver.com>
2025-04-26 16:42:09 +0900

    feat(mainpage): 메인페이지 SpecialProductsSection 추가

--- lib/icons/index.ts
+++ lib/icons/index.ts
@@ -1,4 +1,6 @@
 export { default as AddIcon } from './svg/add.svg';
+export { default as ArrowNextIcon } from './svg/arrow-next.svg';
+export { default as ArrowPrevIcon } from './svg/arrow-prev.svg';
 export { default as BookmarkIcon } from './svg/bookmark.svg';
 export { default as CardBookmarkFilledIcon } from './svg/card-bookmark-filled.svg';
 export { default as CardBookmarkIcon } from './svg/card-bookmark.svg';
