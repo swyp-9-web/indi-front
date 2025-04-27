db1b55e3602f4f2405d76ebaf474eb7d9fa6fd04
LEE. H. J. <73635407+leehj322@users.noreply.github.com>
2025-04-27 16:22:17 +0900

    feat(layout): 글로벌 네비게이션 바 UI 수정 (데이터가 없는 경우) (#18)
    * feat(layout): 팔로잉 작가가 없는 경우 UI 추가
    
    * chore(ui): shadcn/ui popover 추가
    
    * feat(layout): 알림 없음 UI 추가
    
    * refactor(layout): FollowingDropdown → FollowingPopover로 리팩토링

---
+++ app/_components/layout/NotificationPopover.tsx
@@ -0,0 +1,30 @@
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { NotificationIcon, NotificationUnreadIcon } from '@/lib/icons';
+
+export default function NotificationPopover() {
+  const hasUnreadNotification = false;
+
+  return (
+    <Popover>
+      <PopoverTrigger className="flex h-9.5 w-9.5 cursor-pointer items-center justify-center">
+        {hasUnreadNotification ? (
+          <NotificationUnreadIcon className="h-6 w-6" />
+        ) : (
+          <NotificationIcon className="h-6 w-6" />
+        )}
+      </PopoverTrigger>
+      <PopoverContent className="bg-custom-background border-custom-gray-100 z-10 w-80 -translate-x-20 translate-y-1 rounded-none border p-0 shadow-none">
+        <div className="flex h-11.5 w-full items-center justify-between border-b px-5">
+          <h3 className="text-custom-brand-primary text-sm font-medium">읽지 않은 알림 (0)</h3>
+          <button className="text-custom-brand-primary cursor-pointer text-xs font-semibold underline underline-offset-2">
+            모두 읽음
+          </button>
+        </div>
+
+        <div className="text-custom-gray-200 flex h-35 w-full items-center justify-center text-xs">
+          새로운 알림이 없습니다
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
