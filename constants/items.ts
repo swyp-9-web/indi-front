687c0b1367ac37b00aaaaf4e21e91f4c1e72f7fc
leehj322 <gudwns8956@naver.com>
2025-04-30 23:50:38 +0900

    feat(constants): 정렬 관련 아이템 및 관련 유틸 함수 추가

--- constants/items.ts
+++ constants/items.ts
@@ -1,4 +1,5 @@
 // 서버와 프론트엔드 모두에서 사용하는 value-label 매핑 상수입니다.
+// 실제 컴포넌트를 랜더링 또는 프론트에서 로직을 처리할 때 주로 사용됩니다.
 
 export const CATEGORY_ITEMS = [
   { label: '섬유 & 텍스타일 아트', value: 'TEXTILE_ART' },
@@ -16,4 +17,15 @@ export const SIZE_ITEMS = [
   { label: '중형', value: 'M' },
   { label: '소형', value: 'S' },
   { label: '해당없음', value: 'X' },
+] as const;
+
+export const NORMAL_SORT_ITEMS = [
+  { label: '최신순', value: 'CREATED_RECENT' },
+  { label: '인기순', value: 'SCRAPED_TOP' },
+  { label: '과거순', value: 'CREATED_OLDEST' },
+] as const;
+
+export const SCRAP_SORT_ITEMS = [
+  { label: '최신 작품순', value: 'CREATED_RECENT' },
+  { label: '최신 스크랩순', value: 'SCRAPED_RECENT' },
 ] as const;
