42818bc1429724725f7f7f5b1f7aca24a9c08dee
leehj322 <gudwns8956@naver.com>
2025-05-02 01:35:50 +0900

    feat(search): 검색 페이지 헤더 및 검색어 없음 페이지 구현

--- app/(routes)/products/page.tsx
+++ app/(routes)/products/page.tsx
@@ -1,3 +1,19 @@
-export default function Products() {
-  return <div>상품 목록 페이지 (종합)</div>;
+import { notFound } from 'next/navigation';
+
+interface ProductsPageProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function Products({ searchParams }: ProductsPageProps) {
+  const query = await searchParams;
+
+  if (!query?.keyword) return notFound();
+
+  return (
+    <main className="w-8xl mx-auto mt-25 px-20">
+      <h2 className="text-custom-brand-primary text-2xl font-bold">
+        &quot;{query.keyword}&quot; 검색 결과
+      </h2>
+    </main>
+  );
 }
