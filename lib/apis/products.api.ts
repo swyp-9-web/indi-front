33c90ffc381794e3ca290d2fa104779386b5d180
leehj322 <gudwns8956@naver.com>
2025-05-01 19:09:14 +0900

    feat(apis): 상품 리스트 호출 API, `fetchProductsList` 추가

---
+++ lib/apis/products.api.ts
@@ -0,0 +1,32 @@
+import { createQueryParams } from '@/utils/queryParams';
+
+import { ErrorResponse } from './common.type';
+import { ProductsListQueryParams, ProductsListResponse } from './products.type';
+
+export const fetchProductsList = async (
+  queryParams: ProductsListQueryParams
+): Promise<ProductsListResponse> => {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  if (!baseUrl) {
+    throw new Error('API URL이 환경변수에 정의되어 있지 않습니다.');
+  }
+
+  const queryString = createQueryParams(queryParams);
+
+  const res = await fetch(`${baseUrl}/api/v1/items/search?${queryString}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw data as ErrorResponse;
+  }
+
+  return data as ProductsListResponse;
+};
