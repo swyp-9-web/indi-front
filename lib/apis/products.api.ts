156a80c705c80fb2bccabe80828e15272b57bfcc
leehj322 <gudwns8956@naver.com>
2025-05-02 14:56:13 +0900

    feat(proxy): next config의 rewirtes를 사용한 proxy 설정

--- lib/apis/products.api.ts
+++ lib/apis/products.api.ts
@@ -30,3 +30,31 @@ export const fetchProductsList = async (
 
   return data as ProductsListResponse;
 };
+
+export const fetchClientProductsList = async (
+  queryParams: ProductsListQueryParams
+): Promise<ProductsListResponse> => {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  if (!baseUrl) {
+    throw new Error('API URL이 환경변수에 정의되어 있지 않습니다.');
+  }
+
+  const queryString = createQueryParams(queryParams);
+
+  const res = await fetch(`/proxy/api/v1/items/search?${queryString}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw data as ErrorResponse;
+  }
+
+  return data as ProductsListResponse;
+};
