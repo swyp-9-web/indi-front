0302ee85b4217e31136733e07263b7ec1cf0b005
leehj322 <gudwns8956@naver.com>
2025-05-02 17:24:16 +0900

    refactor(shared): 임시 푸터 제거

--- app/(routes)/(main)/page.tsx
+++ app/(routes)/(main)/page.tsx
@@ -1,15 +1,13 @@
 'use client';
 
-import Footer from '@/app/_components/shared/Footer';
 import ScrollToTopButton from '@/app/_components/shared/ScrollToTopButton';
 
-import CategoryFilter from './_components/CategoryFilter';
+import DefaultProductsFilter from './_components/DefaultProductsFilter';
 import DefaultProductsSection from './_components/DefaultProductsSection';
 import HeroSection from './_components/HeroSection';
 import InfiniteProductsSection from './_components/InfiniteProductsSection';
 import SpecialProductsSection from './_components/SpecialProductsSection';
 
-// TODO: 상품 리스트 관련 API 연결 필요
 export default function Home() {
   return (
     <>
@@ -17,7 +15,7 @@ export default function Home() {
 
       <div className="w-8xl mx-auto mt-15 px-21">
         <h2 className="text-custom-brand-primary mb-5 text-2xl font-bold">아르테고의 최신 작품</h2>
-        <CategoryFilter />
+        <DefaultProductsFilter />
       </div>
 
       <DefaultProductsSection />
@@ -25,8 +23,6 @@ export default function Home() {
       <DefaultProductsSection />
       <SpecialProductsSection title="반응 좋은 작품" variant="secondary" />
       <InfiniteProductsSection />
-
-      <Footer className="mt-27.5" />
 
       <ScrollToTopButton />
     </>
