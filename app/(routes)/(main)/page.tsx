fe158b669df4f262ef100012aae9559a9db588b7
LEE. H. J. <73635407+leehj322@users.noreply.github.com>
2025-05-03 01:29:40 +0900

    feat(main): 메인 페이지 API 적용 및 필터링 기능 구현 (#26)
    * refactor(apis): fetchProductsList 함수 parameter 수정
    
    * refactor(main): 필터 토글 그룹 컴포넌트 이름 변경 CategoryFilter → DefaultProductsFilter
    
    * refactor(shared): 임시 푸터 제거
    
    * feat(types): API 요청에 필요한 타입 정의 추가
    
    * feat(main): 메인 페이지 상품 캐러샐 API 연동
    
    * feat(main): 메인 페이지 필터링 기능 zustand 적용
    
    * feat(main): 메인 페이지 필터링 상품 리스트 API 연동
    
    * feat(main): 메인 페이지 무한 스크롤 구현
    
    * refactor(main): prefetch 병렬화 진행 및 margin 값 수정

--- app/(routes)/(main)/page.tsx
+++ app/(routes)/(main)/page.tsx
@@ -1,34 +1,50 @@
-'use client';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
-import Footer from '@/app/_components/shared/Footer';
 import ScrollToTopButton from '@/app/_components/shared/ScrollToTopButton';
+import { fetchProductsList } from '@/lib/apis/products.api';
+import { QUERY_KEYS } from '@/lib/queries/queryKeys';
 
-import CategoryFilter from './_components/CategoryFilter';
-import DefaultProductsSection from './_components/DefaultProductsSection';
+import DefaultProductsFilter from './_components/DefaultProductsFilter';
+import DefaultProductsGrid from './_components/DefaultProductsGrid';
 import HeroSection from './_components/HeroSection';
-import InfiniteProductsSection from './_components/InfiniteProductsSection';
 import SpecialProductsSection from './_components/SpecialProductsSection';
 
-// TODO: 상품 리스트 관련 API 연결 필요
-export default function Home() {
+export default async function Home() {
+  const queryClient = new QueryClient();
+
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: QUERY_KEYS.products.list({ page: 1, limit: 8, sortType: 'SCRAPED_TOP' }),
+      queryFn: async () => await fetchProductsList({ page: 1, limit: 8, sortType: 'SCRAPED_TOP' }),
+      staleTime: 60 * 60 * 1000,
+    }),
+    queryClient.prefetchQuery({
+      queryKey: QUERY_KEYS.products.list({ page: 1, limit: 8, sortType: 'REACTED_TOP' }),
+      queryFn: async () => await fetchProductsList({ page: 1, limit: 8, sortType: 'REACTED_TOP' }),
+      staleTime: 60 * 60 * 1000,
+    }),
+  ]);
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <>
+    <HydrationBoundary state={dehydratedState}>
       <HeroSection />
 
-      <div className="w-8xl mx-auto mt-15 px-21">
+      <div className="w-8xl mx-auto mt-15 mb-17.5 px-20">
         <h2 className="text-custom-brand-primary mb-5 text-2xl font-bold">아르테고의 최신 작품</h2>
-        <CategoryFilter />
+        <DefaultProductsFilter />
       </div>
 
-      <DefaultProductsSection />
-      <SpecialProductsSection title="주목할 만한 작품" variant="primary" />
-      <DefaultProductsSection />
-      <SpecialProductsSection title="반응 좋은 작품" variant="secondary" />
-      <InfiniteProductsSection />
-
-      <Footer className="mt-27.5" />
+      <section>
+        <DefaultProductsGrid page={1} />
+        <SpecialProductsSection title="주목할 만한 작품" variant="primary" sort="SCRAPED_TOP" />
+        <DefaultProductsGrid page={2} />
+        <SpecialProductsSection title="반응 좋은 작품" variant="secondary" sort="REACTED_TOP" />
+        <DefaultProductsGrid page={3} lastGrid />
+      </section>
 
       <ScrollToTopButton />
-    </>
+    </HydrationBoundary>
   );
 }
