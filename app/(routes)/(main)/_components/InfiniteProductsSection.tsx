1f6a4a4a39833b2f85356b617256bb69d206f81f
LEE. H. J. <73635407+leehj322@users.noreply.github.com>
2025-05-04 01:46:13 +0900

    Revert "feat(auth): 로그인 dialog 구현 및 로그인 기능 배포 2차 테스트 (#34)" (#40)
    This reverts commit 8da8bee96cf6ee39f9373343540eec614b9733a3.

---
+++ app/(routes)/(main)/_components/InfiniteProductsSection.tsx
@@ -0,0 +1,24 @@
+import ProductCard from '@/app/_components/product/ProductCard';
+import { productListMock } from '@/lib/mocks/product-list.mock';
+
+export default function InfiniteProductsSection() {
+  const data = productListMock.result.items;
+
+  return (
+    <section className="w-8xl mx-auto mt-15 px-21">
+      <div className="flex w-full gap-x-5">
+        {data.slice(0, 4).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <div className="mt-7.5 flex w-full gap-x-5">
+        {data.slice(4, 8).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <button className="border-custom-gray-100 text-custom-brand-primary mx-auto mt-20 flex h-11.5 w-46 cursor-pointer items-center justify-center rounded-full border text-sm font-medium">
+        더보기
+      </button>
+    </section>
+  );
+}
--- constants/route-paths.ts
+++ constants/route-paths.ts
@@ -10,7 +10,10 @@ export const ROUTE_PATHS = {
   MY_SCRAPPED: '/mypage/products/scrap',
   DELETE_ACCOUNT: '/mypage/account/delete',
 
+  // 로그인/회원가입 관련 (임시)
   // ARTIST_INVITE의 경우 로직에 따라 동적 라우팅이 아니라 쿼리를 이용해서 할 수도 있음
+  LOGIN: '/login',
+  REGISTER: '/register',
   ARTIST_INVITE: (id: string) => `/invite/${id}`,
 
   // 상품 관련 페이지
