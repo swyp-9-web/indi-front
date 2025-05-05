485d4d71d306f0b57288691d107c223561023d09
leehj322 <gudwns8956@naver.com>
2025-05-05 20:18:02 +0900

    feat(queries): 팔로잉 목록 관련 queries 구현

--- lib/queries/queryKeys.ts
+++ lib/queries/queryKeys.ts
@@ -1,12 +1,19 @@
 import { ProductsListQueryParams } from '@/lib/apis/products.type';
 import { stableStringify } from '@/utils/object';
 
+import { FollowingArtistsQueryParams } from '../apis/following.type';
+
 export const QUERY_KEYS = {
   products: {
     list: (queryParams: ProductsListQueryParams) => ['products', stableStringify(queryParams)],
   },
   following: {
     preview: ['following', 'preview'],
+    artists: (queryParams: FollowingArtistsQueryParams) => [
+      'following',
+      'artists',
+      stableStringify(queryParams),
+    ],
   },
   user: {
     summary: ['user', 'summary'],
--- lib/queries/useFollowingQueries.ts
+++ lib/queries/useFollowingQueries.ts
@@ -1,7 +1,16 @@
-import { useMutation, useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { useInfiniteQuery, useMutation, useQuery, UseQueryOptions } from '@tanstack/react-query';
 
-import { fetchFollowingPreview, followArtist, unfollowArtist } from '../apis/following.api';
-import { FollowingPreviewResponse } from '../apis/following.type';
+import {
+  fetchFollowingArtists,
+  fetchFollowingPreview,
+  followArtist,
+  unfollowArtist,
+} from '../apis/following.api';
+import {
+  FollowingArtistsQueryParams,
+  FollowingArtistsResponse,
+  FollowingPreviewResponse,
+} from '../apis/following.type';
 
 import { QUERY_KEYS } from './queryKeys';
 
@@ -23,5 +32,33 @@ export const useToggleFollow = () => {
     mutationFn: async ({ artistId, isFollowing }: { artistId: number; isFollowing: boolean }) => {
       return isFollowing ? await unfollowArtist(artistId) : await followArtist(artistId);
     },
+  });
+};
+
+export const useFollowingArtists = (
+  queryParams: FollowingArtistsQueryParams,
+  options?: Omit<
+    UseQueryOptions<FollowingArtistsResponse, Error, FollowingArtistsResponse>,
+    'queryKey' | 'queryFn'
+  >
+) => {
+  return useQuery({
+    queryKey: QUERY_KEYS.following.artists(queryParams),
+    queryFn: () =>
+      fetchFollowingArtists({ page: 1, limit: 5, ...queryParams }, { runtime: 'client' }),
+    ...options,
+  });
+};
+
+export const useInfiniteFollowingArtists = (queryParams: FollowingArtistsQueryParams) => {
+  return useInfiniteQuery({
+    queryKey: QUERY_KEYS.following.artists(queryParams),
+    queryFn: ({ pageParam = 2 }) =>
+      fetchFollowingArtists({ page: pageParam, ...queryParams }, { runtime: 'client' }),
+    getNextPageParam: (lastPage) => {
+      const { meta } = lastPage.result;
+      return meta.hasNextPage ? meta.currentPage + 1 : undefined;
+    },
+    initialPageParam: 2,
   });
 };
