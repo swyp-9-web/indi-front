1f7d142dde6be3015a2e6fcc2ad3f5247faea2cd
leehj322 <gudwns8956@naver.com>
2025-05-05 12:58:25 +0900

    style(scrap): 스크랩 페이지 스크랩 작품 없음 UI 변경

--- app/(routes)/mypage/scrap/_components/NoScrapProducts.tsx
+++ app/(routes)/mypage/scrap/_components/NoScrapProducts.tsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
+
+import { ROUTE_PATHS } from '@/constants';
+import { FindInPageIcon } from '@/lib/icons';
+
 export default function NoResults() {
   return (
-    <div className="mt-40 flex flex-col items-center justify-center">
-      <h2 className="text-custom-brand-primary mt-4 mb-6 text-center text-2xl font-bold">
-        스크랩한 작품이 없습니다.
+    <div className="mt-20 flex flex-col items-center justify-center">
+      <FindInPageIcon />
+      <h2 className="text-custom-brand-primary mt-4 mb-10 text-center text-2xl font-bold">
+        아직 스크랩 한 작품이 없습니다.
       </h2>
-      <p className="text-custom-gray-400 text-sm">더 많은 작품을 확인해보세요!</p>
+      <Link
+        href={ROUTE_PATHS.HOME}
+        className="border-custom-gray-100 text-custom-brand-primary flex h-11.5 w-46.5 items-center justify-center rounded-full border text-sm font-medium"
+      >
+        작품 둘러보러 가기
+      </Link>
     </div>
   );
 }
