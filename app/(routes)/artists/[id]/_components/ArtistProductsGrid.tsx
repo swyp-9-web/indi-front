848f62b91c471870910c070ac38312632a29d2d7
leehj322 <gudwns8956@naver.com>
2025-05-06 23:42:54 +0900

    feat(artist): 작가 피드 페이지 상품 목록 API 연동

--- app/(routes)/artists/[id]/_components/ArtistProductsGrid.tsx
+++ app/(routes)/artists/[id]/_components/ArtistProductsGrid.tsx
@@ -1,51 +1,33 @@
 import ProductCard from '@/app/_components/product/ProductCard';
+import { fetchProductsList } from '@/lib/apis/products.api';
 
 import NoArtistProducts from './NoArtistProducts';
 import ProductsControls from './ProductsControls';
 
-const mockProduct = {
-  price: 49000,
-  totalScraped: 128,
-  createdAt: '2024-12-10T14:23:00.000Z',
-  updatedAt: '2025-01-15T09:10:00.000Z',
-  id: 1001,
-  thumbnailImgUrl: '',
-  name: '핸드메이드 도자기 머그컵',
-  category: 'CRAFTS',
-  size: 'M',
-  artist: {
-    id: 42,
-    nickname: 'art_maker_woo',
-  },
-  scrap: {
-    isScrapped: true,
-    scrapedAt: '2025-04-01T17:30:00.000Z',
-  },
-  totalReaction: {
-    likes: 230,
-    wants: 180,
-    revisits: 75,
-  },
-};
+interface ArtistProductsGridProps {
+  artistId: number;
+  sortType: string;
+}
 
-export default function ArtistProductsGrid() {
-  const productCount = 0;
+export default async function ArtistProductsGrid({ sortType, artistId }: ArtistProductsGridProps) {
+  const data = await fetchProductsList({ sortType, limit: 15, artistId });
+  const products = data.result.items ?? [];
 
   return (
-    <>
+    <section className="flex-1">
       <div className="mb-2 flex justify-end">
         <ProductsControls />
       </div>
-      {productCount ? (
+
+      {products ? (
         <div className="flex flex-wrap gap-x-5 gap-y-10">
-          <ProductCard product={mockProduct} hasScrapCount />
-          <ProductCard product={mockProduct} hasScrapCount />
-          <ProductCard product={mockProduct} hasScrapCount />
-          <ProductCard product={mockProduct} hasScrapCount />
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} hasScrapCount />
+          ))}
         </div>
       ) : (
         <NoArtistProducts />
       )}
-    </>
+    </section>
   );
 }
--- app/(routes)/artists/[id]/page.tsx
+++ app/(routes)/artists/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import ArtistInfoSection from './_components/ArtistInfoSection';
 import ArtistProductsGrid from './_components/ArtistProductsGrid';
 
@@ -5,24 +7,25 @@ interface CategoryPageProps {
   params: Promise<{
     id: string;
   }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+  searchParams: Promise<{ [key: string]: string | undefined }>;
 }
 
 export default async function Artist({ params, searchParams }: CategoryPageProps) {
-  const { id: artistId } = await params;
-  const { sortType = 'CREATED_RECENT' } = await searchParams;
-
-  console.log(artistId, sortType);
+  const [{ id: artistId }, { sortType }] = await Promise.all([params, searchParams]);
 
   return (
     <main className="w-8xl mx-auto mt-25 flex items-start gap-15 px-20">
-      <section className="relative w-66.25">
-        <ArtistInfoSection />
-      </section>
+      <ArtistInfoSection />
 
-      <section className="flex-1">
-        <ArtistProductsGrid />
-      </section>
+      <Suspense
+        fallback={
+          <div className="mt-25 flex items-center justify-center">
+            <div className="border-custom-gray-200 h-12 w-12 animate-spin rounded-full border-4 border-t-transparent" />
+          </div>
+        }
+      >
+        <ArtistProductsGrid artistId={Number(artistId)} sortType={sortType ?? 'CREATED_RECENT'} />
+      </Suspense>
     </main>
   );
 }
