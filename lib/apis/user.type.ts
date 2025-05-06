deeab06cd8b2f6451db8da711be35679f10d17ad
leehj322 <gudwns8956@naver.com>
2025-05-07 01:08:08 +0900

    feat(apis): 작가 상세 정보 호출 API 함수 구현

--- lib/apis/user.type.ts
+++ lib/apis/user.type.ts
@@ -7,6 +7,24 @@ export interface UserSummary {
   role: 'USER' | 'ARTIST';
 }
 
+export interface ArtistDetail {
+  id: number;
+  profileImgUrl: string;
+  nickname: string;
+  aboutMe: string;
+  isFollowing: boolean;
+  totalItems: number;
+  totalScraps: number;
+  totalReactions: number;
+  totalFollower: number;
+  homeLink: string;
+  snsLinks: string[];
+}
+
 export interface UserSummaryResponse extends SuccessResponse {
   result: UserSummary | null;
+}
+
+export interface ArtistDetailResponse extends SuccessResponse {
+  result: ArtistDetail;
 }
