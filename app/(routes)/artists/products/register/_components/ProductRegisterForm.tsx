4c2ea35d1055e4e5a129f726f7df11962eea9c3f
leehj322 <gudwns8956@naver.com>
2025-05-09 20:30:17 +0900

    feat(register): 상품 등록 상태 반영

--- app/(routes)/artists/products/register/_components/ProductRegisterForm.tsx
+++ app/(routes)/artists/products/register/_components/ProductRegisterForm.tsx
@@ -30,7 +30,7 @@ export default function ProductRegisterForm({
   const [productImages, setProductImages] = useState<(string | File)[]>(initialImgUrls ?? []);
   const [imageOrder, setImageOrder] = useState<string[]>(initialImgUrls ?? []);
 
-  const [submitType, setSubmitType] = useState<'save' | 'upload'>('upload');
+  const [submitType, setSubmitType] = useState<'TEMP' | 'OPEN'>('OPEN');
 
   const form = useForm<FormValues>({
     resolver: zodResolver(productRegisterFormSchema),
@@ -75,7 +75,7 @@ export default function ProductRegisterForm({
       },
 
       // 상품 등록 상태 (OPEN, TEMP 등...)
-      statusType: 'OPEN',
+      statusType: submitType,
 
       // 이미지 파일의 순서를 서버에 전달하기 위한 필드
       imageOrder,
@@ -113,14 +113,14 @@ export default function ProductRegisterForm({
         <div className="mb-20 flex items-center justify-center gap-2.5">
           <button
             type="submit"
-            onClick={() => setSubmitType('save')}
+            onClick={() => setSubmitType('TEMP')}
             className="border-custom-gray-100 flex h-11.5 w-42 cursor-pointer items-center justify-center rounded-full border text-sm font-medium"
           >
             임시저장
           </button>
           <button
             type="submit"
-            onClick={() => setSubmitType('upload')}
+            onClick={() => setSubmitType('OPEN')}
             className="bg-custom-brand-secondary text-custom-gray-900 flex h-11.5 w-42 cursor-pointer items-center justify-center rounded-full text-sm font-medium"
           >
             등록하기
