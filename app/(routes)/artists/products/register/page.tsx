92ff93f561ca19ec9315da415aa22d739f7d2f48
leehj322 <gudwns8956@naver.com>
2025-05-10 17:44:08 +0900

    feat(register): useProductRegisterPage 훅 구현
    - useProductRegisterPage 훅을 이용해서 작품 등록 페이지를 접근하도록 변경
    - 해당 페이지를 벗어나는 경우 mode값 null로 변경

--- app/(routes)/artists/products/register/page.tsx
+++ app/(routes)/artists/products/register/page.tsx
@@ -1,12 +1,39 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import { ROUTE_PATHS } from '@/constants';
+import toast from '@/lib/toast';
+import { useProductRegisterForm } from '@/stores/useProductRegisterForm';
+
 import ProductRegisterForm from './_components/ProductRegisterForm';
 
-// TODO: 동작(수정, 임시저장)에 따라 initialValue를 zustand 혹은 query로 받아서 설정해줘야 함
 export default function RegisterProduct() {
+  const router = useRouter();
+  const { mode, resetFormState } = useProductRegisterForm();
+
+  // useProductRegisterPage 훅을 활용해 접근하지 않은 경우 redirect
+  useEffect(() => {
+    if (!mode) {
+      // 유저 전용 에러 메시지, 뒤로가기로 진입, URL 접근 등 방지
+      router.replace(ROUTE_PATHS.HOME);
+
+      console.error('잘못된 접근입니다. useProductRegisterPage를 통해 접근해주세요.');
+      toast.error('잘못된 접근입니다');
+    }
+
+    return () => resetFormState();
+  }, [mode, router, resetFormState]);
+
+  if (!mode) return null;
+
   return (
     <main className="w-8xl mx-auto mt-25 px-20">
       <h2 className="text-custom-brand-primary mb-12.5 text-2xl font-bold">작품 등록하기</h2>
 
-      <ProductRegisterForm initialValues={null} />
+      <ProductRegisterForm />
     </main>
   );
 }
---
+++ hooks/useProductRegisterPage.ts
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/navigation';
+
+import { ROUTE_PATHS } from '@/constants';
+import { useProductRegisterForm } from '@/stores/useProductRegisterForm';
+
+/**
+ * 상품 등록 페이지를 관리하는 커스텀 훅입니다.
+ *
+ * 반드시 해당 훅을 통헤서 상품 등록 페이지 (`ROUTE_PATHS.REGISTER_PRODUCT`에 진입할 수 있습니다.)
+ */
+export function useProductRegisterPage() {
+  const router = useRouter();
+  const { setMode, setInitialFormValues } = useProductRegisterForm();
+
+  const enterRegisterPage = {
+    create: () => {
+      setMode('CREATE');
+      setInitialFormValues({ initialValues: null, initialImgUrls: [] });
+      router.push(ROUTE_PATHS.REGISTER_PRODUCT);
+    },
+
+    edit: ({ productId }: { productId: number }) => {
+      setMode('EDIT');
+
+      // TODO: productId를 통해 세부 정보를 불러와서 initialValue를 설정
+      console.log(productId);
+
+      router.push(ROUTE_PATHS.REGISTER_PRODUCT);
+    },
+
+    tempEdit: () => {
+      setMode('TEMP_EDIT');
+
+      // 현재 유저 정보를 통해 임시 저장된 값을 불러와서 initialValue를 설정하는 로직 추가 필요
+
+      router.push(ROUTE_PATHS.REGISTER_PRODUCT);
+    },
+  };
+
+  return enterRegisterPage;
+}
