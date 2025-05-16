7f45f8439864ae69b7c0ffc4aa931b6df803e35d
leehj322 <gudwns8956@naver.com>
2025-05-16 23:23:14 +0900

    fix(middleware): 미들웨어 matcher 경로 수정

--- middleware.ts
+++ middleware.ts
@@ -25,5 +25,14 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
 };
