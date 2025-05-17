872f07ffd2ea3e62a204460b9d8e1e0130ee755b
OK <ohl2619@naver.com>
2025-05-17 19:59:03 +0900

    feat(Deatil): 회원 탈퇴 모달 추가

--- app/(routes)/products/[id]/_components/PatchAndDelete.tsx
+++ app/(routes)/products/[id]/_components/PatchAndDelete.tsx
@@ -2,9 +2,19 @@
 
 import { useRouter } from 'next/navigation';
 
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogOverlay,
+  DialogTitle,
+  DialogTrigger,
+} from '@radix-ui/react-dialog';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { Button } from '@/components/ui/button';
+import { DialogFooter, DialogHeader } from '@/components/ui/dialog';
 import { ROUTE_PATHS } from '@/constants/route-paths';
 import { useProductRegisterPage } from '@/hooks/useProductRegisterPage';
 import { deleteProduct } from '@/lib/apis/products.api';
@@ -45,13 +55,42 @@ export default function PatchAndDelete({ itemId, isOwner }: PatchAndDeleteProps)
           >
             작품 수정하기
           </Button>
-          <Button
-            onClick={() => deleteMutation.mutate()}
-            disabled={deleteMutation.isPending}
-            className="text-custom-brand-primary h-auto bg-transparent p-0 shadow-none hover:cursor-pointer hover:bg-transparent hover:underline"
-          >
-            {deleteMutation.isPending ? '삭제 중...' : '작품 삭제하기'}
-          </Button>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="text-custom-brand-primary h-auto bg-transparent p-0 shadow-none hover:cursor-pointer hover:bg-transparent hover:underline">
+                작품 삭제하기
+              </Button>
+            </DialogTrigger>
+            <DialogOverlay className="bg-custom-brand-primary/50 fixed inset-0 z-40" />
+            <DialogContent className="fixed top-1/2 left-1/2 z-50 flex h-113.5 w-100 max-w-md -translate-x-1/2 -translate-y-1/2 flex-col justify-between rounded-xl border bg-white p-7.5">
+              <DialogHeader>
+                <DialogTitle className="text-custom-brand-primary text-2xl font-bold">
+                  작품 삭제
+                </DialogTitle>
+                <DialogDescription className="text-custom-brand-primary fixed top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-[40px] font-bold">
+                  작품을
+                  <br />
+                  삭제하시겠습니까?
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter className="flex gap-2.5">
+                <DialogClose asChild>
+                  <Button className="hover:bg-custom-brand-primary hover:text-custom-background text-custom-brand-primary text-3.5 border-custom-gray-100 rounded-4xl border-[1px] bg-transparent px-17.5 py-3.5 hover:cursor-pointer">
+                    취소
+                  </Button>
+                </DialogClose>
+                <Button
+                  className="bg-custom-gray-100 text-custom-background hover:text-custom-background text-3.5 hover:bg-custom-brand-primary rounded-4xl px-17.5 py-3.5 hover:cursor-pointer"
+                  onClick={() => deleteMutation.mutate()}
+                  disabled={deleteMutation.isPending}
+                >
+                  {/* {deleteMutation.isPending ? '삭제 중...' : '확인'} */}
+                  확인
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       )}
       {!user && <div className="mb-9.5" />}
