94cbb89bc72983ea7f6f5dea92e35fce49f73341
leehj322 <gudwns8956@naver.com>
2025-05-25 00:46:59 +0900

    fix(main): 메인 페이지 prefetch 제거로 인한 hydration error 수정

--- app/(routes)/(main)/page.tsx
+++ app/(routes)/(main)/page.tsx
@@ -1,4 +1,8 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+
 import ScrollToTopButton from '@/app/_components/shared/ScrollToTopButton';
+import { fetchProductsList } from '@/lib/apis/products.api';
+import { QUERY_KEYS } from '@/lib/queries/queryKeys';
 
 import DefaultProductsFilter from './_components/DefaultProductsFilter';
 import DefaultProductsGrid from './_components/DefaultProductsGrid';
@@ -6,8 +10,23 @@ import HeroSection from './_components/HeroSection';
 import SpecialProductsSection from './_components/SpecialProductsSection';
 
 export default async function Home() {
+  const queryClient = new QueryClient();
+
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: QUERY_KEYS.products.list({ page: 1, limit: 8, sortType: 'SCRAPED_TOP' }),
+      queryFn: async () => await fetchProductsList({ page: 1, limit: 8, sortType: 'SCRAPED_TOP' }),
+    }),
+    queryClient.prefetchQuery({
+      queryKey: QUERY_KEYS.products.list({ page: 1, limit: 8, sortType: 'REACTED_TOP' }),
+      queryFn: async () => await fetchProductsList({ page: 1, limit: 8, sortType: 'REACTED_TOP' }),
+    }),
+  ]);
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <>
+    <HydrationBoundary state={dehydratedState}>
       <HeroSection />
 
       <div className="w-8xl mx-auto mt-15 mb-17.5 px-20">
@@ -24,6 +43,6 @@ export default async function Home() {
       </section>
 
       <ScrollToTopButton />
-    </>
+    </HydrationBoundary>
   );
 }
